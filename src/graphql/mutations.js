40b722e231979b1199d953fc39b325472acc488a
James Ballard <jamesballard901@gmail.com>
2020-06-24 14:55:17 -0500

    Changing mutations to use {input}

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,27 +1,34 @@
 import gql from "graphql-tag";
 // const [addArtist, newArtist] = useMutation(ADD_ARTIST);
-const ADD_ARTIST = gql`
-  mutation {
-    addArtist(name: "Andre 3k", genre: "Hip Hop") {
-      id
-      name
-      genre
-    }
-  }
-`;
+// const ADD_ARTIST = gql`
+//   mutation {
+//     addArtist(name: "Andre 3k", genre: "Hip Hop") {
+//       id
+//       name
+//       genre
+//     }
+//   }
+// `;
 
 export const ADD_SONG = gql`
-  mutation {
+  mutation addSong {
     addSong(
-      name: "Lockdown"
-      lyrics: "asdfasdfasdf;lkjasdl;ksadflkjsfadklj;fsadlkj;sfadlk;asfdlkj;asdf"
-      artist: "Anderson .paak"
-      genre: "Hip Hop"
+      input: {
+        name: "War Pigs"
+        artist: "Led Zeppelin"
+        genre: "Rock"
+        lyrics: "Generals gather in their masses..."
+        likes: 0
+        dislikes: 0
+      }
     ) {
+      id
       name
-      lyrics
       artist
       genre
+      lyrics
+      likes
+      dislikes
     }
   }
 `;
